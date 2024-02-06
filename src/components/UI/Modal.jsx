35b7f457d4b90b910c4ed8a9f3d75e4cbc1985d4
laminsurachman <rahman0669@gmail.com>
2024-02-06 11:56:19 +0700

    create dialog Modal

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import { useEffect, useRef } from "react";
+export default function Modal({ children, open, className = "" }) {
+  const dialog = useRef();
+  useEffect(() => {
+    if (open) {
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
